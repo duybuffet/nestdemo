20a01be908d847fe5500c647787143011a818b76
duybuffet <duybuffet@Duys-MacBook-Pro.local>
2021-12-21 15:51:05 +0700

    swagger integrated

--- src/roles/role.entity.ts
+++ src/roles/role.entity.ts
@@ -1,10 +1,13 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity("roles")
 export class Role {
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column()
   name: string;
 }
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -2,33 +2,64 @@ import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common'
 import { CreateRoleDto } from './dto/create-role.dto';
 import { Role } from './role.entity';
 import { RolesService } from './roles.service';
+import { ApiTags, ApiCreatedResponse, ApiOkResponse, ApiResponse, ApiParam } from '@nestjs/swagger';
 
+@ApiTags('roles')
 @Controller('roles')
 export class RolesController {
   constructor(private rolesService: RolesService) {}
 
   @Get()
+  @ApiOkResponse({
+    type: Role,
+    isArray: true
+  })
   index(): Promise<Role[]> {
     return this.rolesService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({
+    type: Role,
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    type: 'number'
+  })
   show(@Param('id') id): Promise<Role> {
     return this.rolesService.findOne(id);
   }
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+    type: Role,
+  })
   create(@Body() role: Role): Promise<Role> {
     return this.rolesService.create(role);
   }
 
   @Put(':id')
+  @ApiParam({
+    name: 'id',
+    required: true,
+    type: 'number'
+  })
+  @ApiResponse({
+    status: 200
+  })
   update(@Param('id') id: number, @Body() role: Role): Promise<any> {
     role.id = id;
     return this.rolesService.update(role);
   }
 
   @Delete(':id')
+  @ApiParam({
+    name: 'id',
+    required: true,
+    type: 'number'
+  })
   delete(@Param('id') id): Promise<any> {
     return this.rolesService.delete(id);
   }
