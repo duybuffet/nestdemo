fb58918c094f64b02895dfeb7eca997ae47b1484
duybuffet <duybuffet@Duys-MacBook-Pro.local>
2021-12-21 14:48:08 +0700

    Add logger

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role } from './role.entity';
 import { Repository, UpdateResult, DeleteResult } from 'typeorm';
 
 @Injectable()
 export class RolesService {
+  private readonly logger = new Logger(RolesService.name);
+
   constructor(
     @InjectRepository(Role)
     private roleRepository: Repository<Role>,
@@ -19,14 +21,17 @@ export class RolesService {
   }
 
   async create(role: Role): Promise<Role> {
+    this.logger.log('Creating role');
     return await this.roleRepository.save(role);
   }
 
   async update(role: Role): Promise<UpdateResult> {
+    this.logger.log('Updating role');
     return await this.roleRepository.update(role.id, role);
   }
 
   async delete(id: number): Promise<DeleteResult> {
+    this.logger.log('Deleting role');
     return await this.roleRepository.delete(id);
   }
 }
