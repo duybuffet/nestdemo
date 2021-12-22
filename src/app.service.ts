3a8100c4b0b879a7bda2eed707d5ae4887fff96b
duybuffet <duybuffet@Duys-MacBook-Pro.local>
2021-12-22 11:19:08 +0700

    Fix default text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World ahihi 222duyyy3!';
+    return 'Hello World!';
   }
 }
